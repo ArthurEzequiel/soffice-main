e533a6cf1150227d390c28faf0096559f6906358
ArthurSantos <98977659+ArthurEzequiel@users.noreply.github.com>
2024-03-13 06:26:09 -0300

    commit

---
+++ js/reveal.js
@@ -0,0 +1,112 @@
+var slideUp = {
+    distance: '15%',
+    origin: 'bottom',
+    delay: 1,
+    duration: 2000,
+    reset: false,
+};
+
+
+ScrollReveal().reveal('.h1-large', slideUp);
+ScrollReveal().reveal('.p-large', {
+    delay: 1,
+    duration: 1500,
+    reset: false,
+    distance: '15%',
+});
+
+ScrollReveal().reveal('.btn-solid-lg', {
+    delay: 1,
+    duration: 1900,
+    reset: false,
+    // distance: '24%',
+});
+
+ScrollReveal().reveal('.btn-outline-lg', {
+    delay: 1,
+    duration: 1500,
+    reset: false,
+    // distance: '20%',
+    
+});
+
+ScrollReveal().reveal('.img-fluid', {
+    delay: 1,
+    duration: 1500,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.intro-text', {
+    delay: 1,
+    duration: 1500,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.text-container', {
+    delay: 1,
+    duration: 1500,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.card-container', {
+    delay: 1,
+    duration: 2000,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.text-quote', {
+    delay: 1,
+    duration: 2000,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.slider-1-container', {
+    delay: 1,
+    duration: 2000,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.img-fluid-contact', {
+    delay: 1,
+    duration: 2000,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.quotes-decoration', {
+    delay: 1,
+    duration: 9000,
+    reset: false,
+    distance: '20%',
+    origin: 'top'
+    
+});
+
+ScrollReveal().reveal('.card', {
+    delay: 1,
+    duration: 2000,
+    reset: true,
+    distance: '10%',
+    origin: 'top'
+    
+});
